fbeabf877b32ab541143734af386b9892ba07338
Vitor Bichara <vitbolb1@gmail.com>
2020-01-12 15:06:33 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+const App: React.FC = () => {
+  const [a, setA] = useState('a');
+
+  useEffect(() => {
+    if (a === 'a') {
+      console.log('do stuff');
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
